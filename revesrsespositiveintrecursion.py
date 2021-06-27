7d680c95af19bda3e24d7c4fa621584b51ba3d3b
bbcoolyeet <66378720+bbcoolyeet@users.noreply.github.com>
2021-06-27 10:06:32 -0700

    Update revesrsespositiveintrecursion.py

--- revesrsespositiveintrecursion.py
+++ revesrsespositiveintrecursion.py
@@ -1,8 +1,9 @@
 def recursenum(number):
+    revs_number = 0  
     while (number > 0):  
         remainder = number % 10  
         revs_number = (revs_number * 10) + remainder  
         number = number // 10  
-revs_number = 0    
-number = 5  
-recursenum(number)
+    return revs_number
+number = 81
+print(recursenum(number))
