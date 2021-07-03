2af9cf6f31dbd8728ffbb69c9dedfcf70f80b865
bbcoolyeet <66378720+bbcoolyeet@users.noreply.github.com>
2021-07-02 21:43:29 -0700

    Add files via upload

---
+++ palidrome.py
@@ -0,0 +1,8 @@
+def ispalindrome(word):
+    if len(word) < 2: 
+        return True
+    if word[0] != word[-1]: 
+        return False
+    return ispalindrome(word[1:-1])
+word = "aba"
+print(ispalindrome(word))
